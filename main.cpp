a0155e376eca1df08dba5e5fcd641b26581c89e6
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-02-11 19:45:26 +0300

    const

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     {
         return code;
     }
-    std :: string user_name = get_name();
+    const std :: string user_name = get_name();
     int attempts_count = check_value(max_value);
 
     const std::string high_scores_filename = "high_scores.txt";
